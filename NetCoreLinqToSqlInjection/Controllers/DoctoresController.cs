5a9c221b6dd90501a84e418a2f8d7d83af05f86a
anitapm03 <apereiramolinero@gmail.com>
2024-02-14 14:25:35 +0100

    A

---
+++ NetCoreLinqToSqlInjection/Controllers/DoctoresController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLinqToSqlInjection.Repositories;
+using NetCoreLinqToSqlInjection.Models;
+
+namespace NetCoreLinqToSqlInjection.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        private IRepositoryDoctores repo;
+
+        public DoctoresController(IRepositoryDoctores repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Doctor> doctores = this.repo.GetDoctores();
+            return View(doctores);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Doctor doctor)
+        {
+            this.repo.InsertarDoctor(doctor.IdDoctor, doctor.Apellido,
+                doctor.Especialidad, doctor.Salario, doctor.IdHospital);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NetCoreLinqToSqlInjection/Models/IRepositoryDoctores.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public interface IRepositoryDoctores
+    {
+        List<Doctor> GetDoctores();
+        void InsertarDoctor
+            (int id, string apellido, string especialidad,
+            int salario, int idHospital);
+    }
+}
