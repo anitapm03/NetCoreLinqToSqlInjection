2e351050c42e548a0b11e7dcd8c3ffda592598b1
anitapm03 <apereiramolinero@gmail.com>
2024-02-14 12:39:20 +0100

    Add project files.

---
+++ NetCoreLinqToSqlInjection/Models/ICoche.cs
@@ -0,0 +1,17 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public interface ICoche
+    {
+
+        //las interfaces no tienen ambito, solamente los
+        //metodos y propiedades qy¡ue debe tener un objeto
+        string Marca { get; set; }
+        string Modelo { get; set; }
+        string Imagen { get; set; }
+        int Velocidad { get; set; }
+        int VelocidadMaxima { get; set; }
+
+        int Acelerar();
+        int Frenar();
+    }
+}
---
+++ NetCoreLinqToSqlInjection/Program.cs
@@ -0,0 +1,41 @@
+using NetCoreLinqToSqlInjection.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddTransient<Coche>();
+//builder.Services.AddSingleton<Coche>();
+//builder.Services.AddSingleton<ICoche,Deportivo>();
+Coche car = new Coche();
+car.Marca = "Mini";
+car.Modelo = "Cooper";
+car.Imagen = "coche.jpg";
+car.Velocidad = 0;
+car.VelocidadMaxima = 300;
+//para poder enviar el obj se usa lambda
+builder.Services.AddSingleton<ICoche, Coche>(x => car);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
